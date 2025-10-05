9cd545594d0f9a5f836479d39d155e1bc71b8313
Estevao Leite <estevaotlnf@gmail.com>
2025-10-05 17:07:54 -0300

    Feat: cria seeder da tabela categoria

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            ['id' => 1, 'nome' => 'Bolos e tortas doces'],
+            ['id' => 2, 'nome' => 'Carnes'],
+            ['id' => 3, 'nome' => 'Aves'],
+            ['id' => 4, 'nome' => 'Peixes e frutos do mar'],
+            ['id' => 5, 'nome' => 'Saladas, molhos e acompanhamentos'],
+            ['id' => 6, 'nome' => 'Sopas'],
+            ['id' => 7, 'nome' => 'Massas'],
+            ['id' => 8, 'nome' => 'Bebidas'],
+            ['id' => 9, 'nome' => 'Doces e sobremesas'],
+            ['id' => 10, 'nome' => 'Lanches'],
+            ['id' => 11, 'nome' => 'Prato Único'],
+            ['id' => 12, 'nome' => 'Light'],
+            ['id' => 13, 'nome' => 'Alimentação Saudável'],
+        ]);
+    }
+}
