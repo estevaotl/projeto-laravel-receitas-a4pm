3ceea4469d1ff96205a98b8973421e25e2ccc768
Estevao Leite <estevaotlnf@gmail.com>
2025-10-08 19:57:42 -0300

    Feat: implementa testes de integração
    # termina configuração do cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8080',
+        baseUrl: 'http://nginx_server',
         specPattern: 'cypress/e2e/**/*.spec.ts',
         supportFile: false,
     },
--- cypress/e2e/home.spec.ts
+++ cypress/e2e/home.spec.ts
@@ -1,7 +1,6 @@
-// cypress/e2e/home.spec.ts
 describe('Home page', () => {
     it('should load and show welcome message', () => {
-        cy.visit('http://localhost:8080');
+        cy.visit('/');
         cy.contains('Bem-vindo');
     });
 });
